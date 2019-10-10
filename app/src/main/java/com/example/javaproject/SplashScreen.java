70a3b03667c4d0db58e7daacd6f9e4b8c9378725
zenofro <frooninckx.zeno@outlook.com>
2019-10-10 14:49:18 +0200

    Splashscreen met camino logo

---
+++ app/src/main/java/com/example/javaproject/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.example.javaproject;
+
+import android.os.Bundle;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
