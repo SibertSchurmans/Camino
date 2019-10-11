d1bdd65757eade85523d4ed41567ac359de8b871
Pieter Coenen <r0668811@ucll.be>
2019-10-11 11:01:51 +0200

    Navigation Implemented

--- app/src/main/java/com/example/javaproject/MapFragment.java
+++ app/src/main/java/com/example/javaproject/MapFragment.java
@@ -1,6 +1,9 @@
 package com.example.javaproject;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +17,34 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.internal.PolylineEncoding;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback,
+        GoogleMap.OnPolylineClickListener, GoogleMap.OnInfoWindowClickListener{
     private GoogleMap mMap;
     private SupportMapFragment mapFragment;
+    private GeoApiContext mGeoApiContext = null;
+    private static final String TAG = "marker";
+    private ArrayList<PolylineData> mPolylinesData = new ArrayList<>();
+    private Marker mSelectedMarker = null;
+
 
     public MapFragment(){
 
@@ -49,10 +71,139 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         LatLng genk = new LatLng(50.957502, 5.482947);
         LatLng echteHerk = new LatLng(50.956471, 5.183986);
-        mMap.addMarker(new MarkerOptions().position(genk).title("Marker in genk"));
-        mMap.addMarker(new MarkerOptions().position(echteHerk).title("Marker in schulen"));
+        //mMap.addMarker(new MarkerOptions().position(genk).title("Marker in genk"));
+        //mMap.addMarker(new MarkerOptions().position(echteHerk).title("Marker in schulen"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(genk));
+
+        MarkerOptions pos1 = new MarkerOptions().position(genk).title("genk");
+        MarkerOptions pos2 = new MarkerOptions().position(echteHerk).title("herk");
+
+        mMap.addMarker(pos1);
+        mMap.addMarker(pos2);
+
+        if(mGeoApiContext == null){
+            mGeoApiContext = new GeoApiContext.Builder()
+                    .apiKey(getString(R.string.google_maps_key))
+                    .build();
+        }
+
+        calculateDirections(pos1, pos2);
     }
 
 
+    private void calculateDirections(MarkerOptions origin, MarkerOptions dest){
+
+
+        com.google.maps.model.LatLng destination = new com.google.maps.model.LatLng(
+                dest.getPosition().latitude,
+                dest.getPosition().longitude
+        );
+
+
+        DirectionsApiRequest directions = new DirectionsApiRequest(mGeoApiContext);
+
+        directions.alternatives(true);
+        directions.origin(
+                new com.google.maps.model.LatLng(
+                        origin.getPosition().latitude,
+                        origin.getPosition().longitude
+                )
+        );
+
+        directions.destination(destination).setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                Log.d(TAG, "onResult: routes: " + result.routes[0].toString());
+                Log.d(TAG, "onResult: geocodedWayPoints: " + result.geocodedWaypoints[0].toString());
+
+                addPolylinesToMap(result);
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                Log.e(TAG, "onFailure: " + e.getMessage() );
+
+            }
+        });
+    }
+
+    private void addPolylinesToMap(final DirectionsResult result){
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "run: result routes: " + result.routes.length);
+
+                if (mPolylinesData.size() > 0){
+                    for (PolylineData polylineData : mPolylinesData){
+                        polylineData.getPolyline().remove();
+                    }
+                    mPolylinesData.clear();
+                    mPolylinesData = new ArrayList<>();
+                }
+                double duration = 9999999;
+                for(DirectionsRoute route: result.routes){
+                    Log.d(TAG, "run: leg: " + route.legs[0].toString());
+                    List<com.google.maps.model.LatLng> decodedPath = PolylineEncoding.decode(route.overviewPolyline.getEncodedPath());
+
+                    List<LatLng> newDecodedPath = new ArrayList<>();
+
+                    // This loops through all the LatLng coordinates of ONE polyline.
+                    for(com.google.maps.model.LatLng latLng: decodedPath){
+
+//                        Log.d(TAG, "run: latlng: " + latLng.toString());
+
+                        newDecodedPath.add(new LatLng(
+                                latLng.lat,
+                                latLng.lng
+                        ));
+                    }
+                    Polyline polyline = mMap.addPolyline(new PolylineOptions().addAll(newDecodedPath));
+                    polyline.setColor(getResources().getColor(R.color.Gray));
+                    polyline.setClickable(true);
+                    mPolylinesData.add(new PolylineData(polyline, route.legs[0]));
+
+                    double temDuration = route.legs[0].duration.inSeconds;
+                    if(temDuration < duration){
+                        duration = temDuration;
+                        onPolylineClick(polyline);
+                    }
+                    mSelectedMarker.setVisible(false);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public void onPolylineClick(Polyline polyline) {
+        int index = 0;
+        for(PolylineData polylineData: mPolylinesData){
+            Log.d(TAG, "onPolylineClick: toString: " + polylineData.toString());
+            if(polyline.getId().equals(polylineData.getPolyline().getId())){
+                polylineData.getPolyline().setColor(getResources().getColor(R.color.lightBlue));
+                polylineData.getPolyline().setZIndex(1);
+
+                LatLng endLocation = new LatLng(
+                        polylineData.getLeg().endLocation.lat,
+                        polylineData.getLeg().endLocation.lng
+                );
+
+                Marker marker = mMap.addMarker(new MarkerOptions()
+                        .position(endLocation)
+                        .title("Route #" + index)
+                        .snippet("Duration: " + polylineData.getLeg().duration)
+                );
+
+                marker.showInfoWindow();
+            }
+            else{
+                polylineData.getPolyline().setColor(getResources().getColor(R.color.Gray));
+                polylineData.getPolyline().setZIndex(0);
+            }
+        }
+    }
 }
